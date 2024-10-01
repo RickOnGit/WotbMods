9700530bc54e3bff2eadd23139c5433f2200b8d8
rick <riccardoguadagnini7@gmail.com>
2024-10-01 15:24:26 +0200

    adding variables.sh ad backup.sh

---
+++ Installation/backup.sh
@@ -0,0 +1,10 @@
+import variables.sh
+
+B_3d() {
+    cp -rf "$F1" "$B1"
+}
+
+B_WwiseSound_XML () {
+    cp -rf "$F2" "$B2"
+    cp -rf "$F3" "$B2"
+}
---
+++ Installation/variables.sh
@@ -0,0 +1,10 @@
+B1="$HOME/FullDataFolderBackup"
+
+B2="$HOME/OgSounds"
+
+F1="$HOME/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data"
+
+F2="$HOME/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/WwiseSound"
+
+F3="$HOME/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/XML"
+
