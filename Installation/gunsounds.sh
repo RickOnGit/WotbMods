aad103bd6a3ce59b1e0446919a1041c17e3bcd39
rick <riccardoguadagnini7@gmail.com>
2024-10-02 13:33:13 +0200

    updates

---
+++ Installation/gunsounds.sh
@@ -0,0 +1,5 @@
+source variables.sh
+
+gunsounds1() {
+		 
+}
---
+++ Installation/variables.sh
@@ -0,0 +1,10 @@
+export f1=""$HOME"/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data"
+
+export f2=""$HOME"/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/WwiseSound"
+export f3=""$HOME"/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/XML"
+
+export f4=""$HOME"/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/Configs"
+
+export f5=""$HOME"/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/Gfx"
+
+export b1=""$HOME"/GunSoundsBackup"
