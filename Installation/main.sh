f7df5dffd33c8455035b6d54de67e89186ef9502
rick <riccardoguadagnini7@gmail.com>
2024-10-22 12:04:19 +0200

    adding general folder and editing the main menu

--- Installation/main.sh
+++ Installation/main.sh
@@ -1,31 +1,42 @@
-source ./colors.sh
-source ./functions.sh
+source general/./colors.sh
+source general/./logo.sh
+#source ./functions.sh
 
 while true 
 do
-    echo -e ""$CYAN"(?) "$YELLOW"Chose what to do :"$NC"\n"
-    echo -e "(A) Merge all files\t (B) Merge all files but not the 3d/tank folder\t (C) Chose which remodel to load\t (Q) Quit the program"
-
-    echo -e -n "\n"$CYAN"(?) "$YELLOW"Chose an option [A-Q]: "$NC""
+    show_logo
+    tput cup 2 50
+    echo -e "$BOLD$CYAN(?)$YELLOW Chose what to do :"$NC"\n"
+    tput cup 4 50 
+    echo -e "(A) Merge all files\n"
+    tput cup 6 50
+    echo -e "(B) Merge all files but not the 3d/tank folder\n"
+    tput cup 8 50
+    echo -e "(C) Chose which remodel to load\n"
+    tput cup 10 50
+    echo -e "(Q) Quit the program\n"
+    tput cup 12 50
+    echo -e -n ""$BOLD""$CYAN"(?) "$YELLOW" Chose an option [A-Q]: "$NC""
     read ans
+    tput cup 16
 
-    if [[ "$ans" == "A" || "$ans" == "a" ]]; then
-        merge_all
-    fi
-
-    if [[ "$ans" == "B" || "$ans" == "b" ]]; then
-        merge_no_tanks
-    fi
-
-    if [[ "$ans" == "C" || "$ans" == "c" ]]; then
-        chose_remodel
-    fi
-
-    if [[ "$ans" == "D" || "$ans" == "d" ]]; then
-        echo -e "\nQuitting the script...\n"
-        break
-    fi
+    case $ans in
+        A)        
+            #merge all files
+            ;;
+        B)
+            #merge all files but no tanks
+            ;;
+        C)
+            #chose the remodel to be loaded
+            ;;
+        Q)
+            echo -e "\nQuitting the script..."
+            break
+            ;;
+        *)
+            echo -e "Invalid choice, retry..."
+            ;;        
+    esac
     clear
 done
-
-#Domande: Considerando la dimensione della repository che altre opzioni posso implementare per scaricare automaticamente i vari file?
