13d2a5a09a87aec18c311f5afa3ca120c75f57e5
rick <riccardoguadagnini7@gmail.com>
2024-10-23 10:17:59 +0200

    Adding fixes

--- Remodels/Tanks/German/E-50M/nimmermer/nimmermer.sh
+++ Remodels/Tanks/German/E-50M/nimmermer/nimmermer.sh
@@ -1,4 +1,5 @@
-source ../../.././variables.sh
+source $HOME/Documents/WotbMods/Remodels/./variables.sh
+#impostare source su home e non su socuments
 wget https://forblitz.ru/wp-content/uploads/2022/12/nimmermer_steam.zip
 unzip nimmermer_steam.zip > /dev/null 2>&1
 cp Data/3d/Tanks/German/*.sc2.dvpl mask && cp Data/3d/Tanks/German/*.scg.dvpl mask
