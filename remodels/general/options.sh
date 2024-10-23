c072a1f84d1640856de7f624c52df36a230acba5
rick <riccardoguadagnini7@gmail.com>
2024-10-24 00:21:45 +0200

    deleted some files

--- remodels/general/options.sh
+++
@@ -1,8 +0,0 @@
-while true
-do {
-    echo -e "$BOLD$CYAN(?)$YELLOW Chose what to do :"$NC"\n"
-    echo -e "(A) Tanks Remodeling\n"
-    echo -e "(B) Hangar restyle\n"
-    echo -e "(C) Maps restyle\n"
-done
-}
