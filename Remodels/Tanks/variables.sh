22848bebb9b105fd5eeb46ae723ca75df06f2f20
rick <riccardoguadagnini7@gmail.com>
2024-10-23 12:13:11 +0200

    Adding fixes

--- Remodels/Tanks/variables.sh
+++
@@ -1,3 +0,0 @@
-export WgDataFlat=$HOME/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/
-export WgDataRpm=$HOME/.local/share/Steam/steamapps/common/'World of Tanks Blitz'/Data/
-RickData="$HOME/WotbMods/Data"
