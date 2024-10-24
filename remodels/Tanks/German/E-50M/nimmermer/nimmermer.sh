b671edc5e29239657d0f330f68f8918a75d675ab
rick <riccardoguadagnini7@gmail.com>
2024-10-24 16:29:07 +0200

    implemented the remodel switch for german tanks

--- remodels/Tanks/German/E-50M/nimmermer/nimmermer.sh
+++ remodels/Tanks/German/E-50M/nimmermer/nimmermer.sh
@@ -1,5 +1,4 @@
 source $HOME/RickWotbMods/general/./variables.sh
-#impostare source su home e non su socuments
 wget https://forblitz.ru/wp-content/uploads/2022/12/nimmermer_steam.zip
 unzip nimmermer_steam.zip > /dev/null 2>&1
 mkdir -p mask
@@ -17,3 +16,4 @@ fi
 rm nimmermer_steam.zip && rm -rf Data
 
 echo -e "\nDONE!\n"
+
